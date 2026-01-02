fc05dbb388f419ccd43ce243e4691b37af34f9d9
Gun8hoot <gun8hoot@proton.me>
2026-01-02 17:10:55 +0100

    ...

--- lv2/n_queen/nqueen.c
+++ lv2/n_queen/nqueen.c
@@ -11,33 +11,60 @@ void  print_pos(int *arr, int n)
 	}
 }
 
-int nqueen(int *arr_int, int n, int col)
+int	  str_is_number(char *str)
 {
-	if (col == n)
-		print_pos(arr_int, n)
-	else 
+	for (int i = 0;str[i]; i++)
 	{
-	
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
 	}
+	return (1);
 }
 
-int queen_is_safe(int row, int col, )
+int	  check_queen(int *arr, int pos_y, int pos_x, int size)
 {
+	for (int i = 0; i < pos_y; i++)
+	{
+		if (arr[i] == pos_x)	  // Check colums/row
+			return (0);
+		else if (pos_y - i == pos_x - arr[i] || pos_y + i == pos_x + arr[i]) // Check diagonal both direction
+			return (0);
+	}
+	return (1);
+}
 
+void nqueen(int *arr_pos, int col, int row, int size)
+{
+	if (col == size)
+	{
+		print_pos(arr_pos, size);
+		return ;
+	}
+	for (int x = 0; x < size; x++)
+	{
+		if (check_queen(arr_pos, col, x, size))
+		{
+			arr_pos[col] = x;
+			nqueen(arr_pos, col + 1, x, size);
+		}
+	}	
+	return ;
 }
 
 int	main(int argc, char **argv)
 {
-	int *pos = NULL;
-	int n;
+	int size;
+	int *pos;
 
-	if (argc != 2)
+	if (argc != 2 || !str_is_number(argv[1]))
 		return (1);
-	n = atoi(argv[1]);
-	pos = calloc(n, sizeof(int));
+	size = atoi(argv[1]);
+	if (size <= 0)
+		return (1);
+	pos = calloc(size, sizeof(int));
 	if (!pos)
 		return (0);
-	nqueen(pos, n, 0)
+	nqueen(pos, 0, 0, size);
 
 	return (0);
 }
--- lv2/n_queen/nqueen.h
+++ lv2/n_queen/nqueen.h
@@ -5,20 +5,4 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-typedef struct s_queen
-{
-	int n
-	int pos_row;
-	int pos_collum;
-	t_queen *next;
-} t_queen;
-
-typedef struct s_holder
-{
-	int queen_in_struct;
-	int sum_queen;
-	int salt;
-	t_queen **queen;
-} t_holder;
-
 #endif // !nqueen
